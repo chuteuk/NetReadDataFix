d2d2c6cf059a3e5a29ec8f0328eaffa722e4f424
chuteuk <devsparks_14@hotmail.com>
2017-02-16 01:57:09 +0000

    Initial upload

---
+++ lua/autorun/server/net_readdatafix.lua
@@ -0,0 +1,5 @@
+--Override the old net.ReadData function
+local oldNetReadData = oldNetReadData or net.ReadData
+function net.ReadData(len)
+	return oldNetReadData(math.Clamp(len,0,65533)) --Clamp the length to the maximum size of a net message (65533 bytes)
+end
---
+++ readme.md
@@ -0,0 +1,5 @@
+# net.ReadData() Crash Fix
+
+Stops people crashing servers running with Sandbox as the base gamemode or using other addons which utilize net.ReadData
+
+No configuration required, just download unzip and drag and drop into addons
